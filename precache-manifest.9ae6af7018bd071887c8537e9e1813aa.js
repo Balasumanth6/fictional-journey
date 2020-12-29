e54cc90f0899dbf05aa206e995f3fddbcb618b25
Balasumanth <beldebalasumanth6@gmail.com>
2020-12-29 16:03:20 +0530

    Updates

---
+++ precache-manifest.9ae6af7018bd071887c8537e9e1813aa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592e64248c50283058fae2b6fefb709b",
+    "url": "/fictional-journey/home/index.html"
+  },
+  {
+    "revision": "a9850322b13f5a94523f",
+    "url": "/fictional-journey/home/static/css/2.90d2301a.chunk.css"
+  },
+  {
+    "revision": "604dff822f4ad09ba37b",
+    "url": "/fictional-journey/home/static/css/main.a37c2842.chunk.css"
+  },
+  {
+    "revision": "a9850322b13f5a94523f",
+    "url": "/fictional-journey/home/static/js/2.b1354bbd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fictional-journey/home/static/js/2.b1354bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604dff822f4ad09ba37b",
+    "url": "/fictional-journey/home/static/js/main.f80c17b8.chunk.js"
+  },
+  {
+    "revision": "116d5093e246c2410860",
+    "url": "/fictional-journey/home/static/js/runtime-main.0488cc9a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fictional-journey/home/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fictional-journey/home/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fictional-journey/home/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fictional-journey/home/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fictional-journey/home/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "eb267f35256591eb7733b73c0455dbfe",
+    "url": "/fictional-journey/home/static/media/logo.eb267f35.png"
+  }
+]);
